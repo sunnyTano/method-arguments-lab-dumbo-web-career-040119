23620ef6231b39f425a6863c7f0b113796e3a1cb
Susan Tan <ssusantan9@gmail.com>
2019-03-12 01:18:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-# def introduction(name)
-#   puts "Hi, my name is #{name}."
-# end
-
-def introduction_with_language(name, language)
-  puts "Hi, my names is #{name} and I am learning to program in #{language."
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
+
+# def introduction_with_language(name, language)
+#   puts "Hi, my names is #{name} and I am learning to program in #{langua."
+# end
